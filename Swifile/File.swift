245862074eda38f131be21c32bd30ba222c2e857
Speedyfriend67 <82425907+speedyfriend67@users.noreply.github.com>
2024-03-15 16:57:17 +0900

    Update File.swift

--- Swifile/File.swift
+++ Swifile/File.swift
@@ -1,8 +1,37 @@
 import Foundation
 
-func contentsOfDirectory(_ Path: String) -> [String] {
-    let test = runHelper(["l", Path])
+func contentsOfDirectory(_ path: String) -> [String] {
+    let test = runHelper(["l", path])
     return (test.status != 0 || test.output == "") ? [] : test.output.components(separatedBy: "\n")
+}
+func createFile(atPath path: String, contents: Data?, attributes: [FileAttributeKey : Any]? = nil) -> Bool {
+    return FileManager.default.createFile(atPath: path, contents: contents, attributes: attributes)
+}
+
+func createDirectory(atPath path: String, withIntermediateDirectories createIntermediates: Bool = true, attributes: [FileAttributeKey : Any]? = nil) throws {
+    try FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: createIntermediates, attributes: attributes)
+}
+
+func removeItem(atPath path: String) throws {
+    try FileManager.default.removeItem(atPath: path)
+}
+
+func removeDirectory(atPath path: String) throws {
+    try FileManager.default.removeItem(atPath: path)
+}
+func overwriteFile(atPath path: String, withContents contents: Data) -> Bool {
+    if FileManager.default.fileExists(atPath: path) {
+        do {
+            try contents.write(to: URL(fileURLWithPath: path))
+            return true
+        } catch {
+            print("Error overwriting file: \(error)")
+            return false
+        }
+    } else {
+        print("File does not exist at path: \(path)")
+        return false
+    }
 }
 
 extension Array: RawRepresentable where Element: Codable {
